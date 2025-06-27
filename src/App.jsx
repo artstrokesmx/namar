1d04479040417d35902c9c7c6164af8343570864
Miguel Arturo García Miranda <psic.arturomiranda@gmail.com>
2025-06-27 12:16:45 -0600

    coreccion navegación

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Inicio } from './paginas/Inicio'
 import { Categorias } from './componentes/Categorias'
 import { CarritoPagina } from './paginas/CarritoPagina'
-import { Navegacion } from './componentes/Navegacion'
 
 function App() {
   return (
     <Router basename="/namar">
-      <Navegacion />
       <Routes>
         <Route index element={<Inicio />} /> {/* Ruta exacta para /namar/ */}
         <Route path="/" element={<Inicio />} />
